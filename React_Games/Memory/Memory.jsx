3ddc3978cbe36dd487aea3e52897081c6349f105
drmeloy <drmeloy@gmail.com>
2020-04-20 05:11:58 -0700

    working on react memory game

---
+++ React_Games/Memory/Memory.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import cardsArray from './cards.js';
+
+export default function MemoryGame(){
+  const [boardSize, setBoardSize] = useState('4x3');
+
+  
+
+  return (
+    <div>
+
+    </div>
+  );
+}
---
+++ React_Games/Memory/cards.js
@@ -0,0 +1,50 @@
+export const cardArray = [
+  {
+    name: 'fries',
+    img: './assets/fries.png'
+  },
+  {
+    name: 'fries',
+    img: './assets/fries.png'
+  },
+  {
+    name: 'cheeseburger',
+    img: './assets/cheeseburger.png'
+  },
+  {
+    name: 'cheeseburger',
+    img: './assets/cheeseburger.png'
+  },
+  {
+    name: 'hotdog',
+    img: './assets/hotdog.png'
+  },
+  {
+    name: 'hotdog',
+    img: './assets/hotdog.png'
+  },
+  {
+    name: 'ice-cream',
+    img: './assets/ice-cream.png'
+  },
+  {
+    name: 'ice-cream',
+    img: './assets/ice-cream.png'
+  },
+  {
+    name: 'milkshake',
+    img: './assets/milkshake.png'
+  },
+  {
+    name: 'milkshake',
+    img: './assets/milkshake.png'
+  },
+  {
+    name: 'pizza',
+    img: './assets/pizza.png'
+  },
+  {
+    name: 'pizza',
+    img: './assets/pizza.png'
+  },
+];
